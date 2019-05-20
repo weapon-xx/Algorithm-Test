d0ddca3e8c33f86225875e5d24df2760b23e8d1a
jaccksonX <88180569@qq.com>
2019-05-20 22:20:34 +0800

    feat: add 22 generate-parentheses

---
+++ LeetCode/22.generate-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * 给出 n 代表生成括号的对数，请你写出一个函数，
+ * 使其能够生成所有可能的并且有效的括号组合。
+ * 例如，给出 n = 3，生成结果为：
+ * ["((()))","(()())","(())()","()(())","()()()"]
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+	const res = [];
+	function generate(str = '', left = 0, right = 0) {
+		if (str.length === 2 * n) {
+			res.push(str);
+			return ;
+		}
+		if (left < n) {
+			generate(str + '(', left + 1, right);
+		}
+		if (right < left) {
+			generate(str + ')', left, right + 1);
+		}
+	}
+	generate();
+	return res;
+};
+
+console.log(generateParenthesis(3));
