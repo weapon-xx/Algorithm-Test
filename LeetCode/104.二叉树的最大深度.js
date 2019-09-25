e85be616348a39980124f6340fffa1468c27d659
jaccksonX <88180569@qq.com>
2019-09-25 22:27:53 +0800

    feat: add 104.二叉树的最大深度

---
+++ LeetCode/104.二叉树的最大深度.js
@@ -0,0 +1,39 @@
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+    if (!root) {
+        return 0;
+    }
+    function deep(node, level) {
+        if (node.val !== null) {
+            level += 1;
+        }
+        const left = node.left ? deep(node.left, level) : level;
+        const right = node.right ? deep(node.right, level) : level;
+        return Math.max(left, right);
+    }
+    return deep(root, 0);
+}
+
+console.log(maxDepth());
