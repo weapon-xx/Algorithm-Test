8ae9a005a5e3d9703a69c6aff419fafca4f20f3d
jaccksonX <88180569@qq.com>
2019-09-29 22:51:44 +0800

    feat: add 110.平衡二叉树

---
+++ LeetCode/110.平衡二叉树.js
@@ -0,0 +1,41 @@
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * 实例1：
+ * 给定二叉树 [3,9,20,null,null,15,7]，返回 true 。
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isBalanced(root) {
+    if (!root) return true;
+
+    function getMaxDepth(node, level) {
+        if (!node) {
+            return level;
+        }
+
+        const left = node.left ? getMaxDepth(node.left, level + 1) : level;
+        const right = node.right ? getMaxDepth(node.right, level + 1) : level;
+        if (left === -1 || right === -1) {
+            return -1;
+        }
+        return Math.abs(left - right) < 2 ? Math.max(left, right) : -1;
+    }
+
+    const left = root.left ? getMaxDepth(root.left, 1) : 0;
+    const right = root.right ? getMaxDepth(root.right, 1) : 0;
+    return left !== -1 && right !== -1 ? Math.abs(left - right) < 2 : false;
+}
+
+console.log(isBalanced());
