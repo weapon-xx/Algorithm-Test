5598969d1ae830d5df6544ae14fbb67fd386c877
jaccksonX <88180569@qq.com>
2019-11-14 15:06:56 +0800

    feat: update 26

---
+++ LeetCode/26.删除排序数组中的重复项.js
@@ -0,0 +1,26 @@
+/**
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 示例 1:
+ * 给定数组 nums = [1,1,2],
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+    // 注意有一个要求，不使用额外空间，并且返回新长度
+    let j = 0;
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] !== nums[j]) {
+            j += 1;
+            nums[j] = nums[i];
+        }
+    }
+    j += 1;
+    return j;
+}
+
+console.log(removeDuplicates([1, 1, 2])); // 2
